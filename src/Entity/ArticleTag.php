ce7d6c87e69ddc2fea0e68ba519cfd69b0ca2e7a
Daniel Boling <dbdanielbboling70@gmail.com>
2024-03-06 16:57:58 -0500

    progress save - broken

--- src/Entity/ArticleTag.php
+++ src/Entity/ArticleTag.php
@@ -5,7 +5,6 @@ namespace App\Entity;
 use App\Repository\ArticleTagRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
-use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use \DateTime;
 
